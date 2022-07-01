02aeb6bb5ade8b94f5e6f6256bcddc6e4badeaa0
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-01 09:11:29 -0700

    this is a if_else_while loop commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d", n, n % 10, n % 10);
+	printf("Last digit of %d is ", n);
+	printf("%d ", n % 10);
 	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,15 +6,15 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	int a;
 
-	for (a = 48;a < 58; a++)
+	for (a = 48; a < 58; a++)
 	{
 		putchar(a);
 	}
-	for (a = 97;a < 103;a++)
+	for (a = 97; a < 103; a++)
 	{
 		putchar(a);
 	}
