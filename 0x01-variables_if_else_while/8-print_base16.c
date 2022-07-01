2fbd47ce0a90e0b10c2d185f258904fd7f6b608e
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-01 08:20:43 -0700

    this is a if_else_while loop commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int a;
+
+	for(a = 48;a < 58; a++)
+	{
+		putchar(a);
+	}
+	for(a = 65;a<71;a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+/**
+ * main -prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for(a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0) ;
+}
