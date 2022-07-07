5eaef7fac8777c38bd68c90bbbaa0b10f726449f
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-06 18:07:47 -0700

    This is a nested loops commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main -  prints _putchar, 
- * followed by a new line
+ * main - prints _putchar,followed by a new line
+ * returns 0
  * Return - Always 0 (Success)
  */
 
