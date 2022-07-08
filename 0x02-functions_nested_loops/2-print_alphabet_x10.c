26a5fa2bff3fe079dab06acaaedfad02c0324e7e
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-07 17:12:25 -0700

    This is a nested loops commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 10;
+
+	while (a <10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
