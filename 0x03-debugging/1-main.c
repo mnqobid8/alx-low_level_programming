f4065b10938cc7a7266e013f87759fac9ac868bd
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-07 18:36:21 -0700

    added task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	/*@i: is not incremented so remains as 0 hence i is always less than 
+	 * 10 and the while loops is infinite
+	 */ 
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+
