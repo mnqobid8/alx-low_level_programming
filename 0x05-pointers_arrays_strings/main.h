df22dffa8a5ec0e0bf4f194242763d028f3b59cf
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-11 18:04:39 -0700

    made changes to main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
-_putchar.c;
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar.c(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +12,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif
