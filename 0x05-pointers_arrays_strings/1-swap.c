5a2b819a7498008df5fb1efa0c38ac1d65af6e12
mnqobid8 <mnqobidlamini476@gmail.com>
2022-07-11 18:23:56 -0700

    added task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: variable to be swapped 
+ * @b: variable to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int val;
+
+	val = *a;
+	*a = *b;
+	*b = val;
+}
